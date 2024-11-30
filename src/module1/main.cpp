1ab3c1730513dcad120ab3981c2d41d2426c5b7e
Web4 <167559384+Web4application@users.noreply.github.com>
2024-11-30 01:05:05 -0500

    Update main.cpp

--- src/module1/main.cpp
+++ src/module1/main.cpp
@@ -1,47 +1,12 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+#include "CRAXExpr.h"
+#include <iostream>
 
-#include <flutter/dart_project.h>
-#include <flutter/flutter_view_controller.h>
-#include <windows.h>
+int main() {
+// Example usage of BaseOffsetExpr
+auto base = klee::ConstantExpr::create(0x400000, klee::Expr::Int64);
+auto offset = klee::ConstantExpr::create(0x1000, klee::Expr::Int64);
+auto expr = klee::BaseOffsetExpr::alloc(base, offset, "base", "offset");
 
-#include "flutter_window.h"
-#include "utils.h"
-
-int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    CreateAndAttachConsole();
-  }
-
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
-  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-
-  flutter::DartProject project(L"data");
-
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
-
-  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
-
-  FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"complex_layout", origin, size)) {
-    return EXIT_FAILURE;
-  }
-  window.SetQuitOnClose(true);
-
-  ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
-    ::TranslateMessage(&msg);
-    ::DispatchMessage(&msg);
-  }
-
-  ::CoUninitialize();
-  return EXIT_SUCCESS;
+std::cout << "Expression: " << expr->toString() << std::endl;
+return 0;
 }
