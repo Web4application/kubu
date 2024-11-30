c207580bc01217e57ea28cc23f0e5ba4f857dd4b
Web4 <167559384+Web4application@users.noreply.github.com>
2024-11-30 00:34:00 -0500

    Update package.js

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ let package = Package(
 import PackageDescription
 
 let package = Package(
-name: "MyVaporApp",
+name: "kubu-hai",
 dependencies: [
 .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
 .package(url: "https://github.com/vapor/queues.git", from: "1.0.0")
