24921d7f912cc3ab15375e5f85575b31c6eae579
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-19 17:27:39 -0700

    Update build.swift

--- build.swift
+++ build.swift
@@ -2,17 +2,17 @@ import PackageDescription
 
 let package = Package(
   name: "roda",
-  platforms: [.macOS(.v13)],
+  platforms: [.macOS(.v19)],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-    .package(name: "GoogleGenerativeAI", path: "../../"),
+    .package(name: "web4GenerativeAI", path: "../../"),
   ],
   targets: [
     .executableTarget(
       name: "generate-content",
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
-        .product(name: "GoogleGenerativeAI"// swift-tools-version:5.2
+        .product(name: "web4GenerativeAI"// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
