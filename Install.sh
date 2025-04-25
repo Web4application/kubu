4b6ee06f3050d66fc69a4ea3083f1db8816103e2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 05:51:22 -0700

    Update Install.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Install.sh
+++ Install.sh
@@ -3,3 +3,4 @@ bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEA
 npm i --save genkit @genkit-ai/googleai
 pip install python-dotenv psycopg2
 pip install vocos[train]
+pip install -r requirements.txt
