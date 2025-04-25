6871935eb370a26b705564d74c0b1bbaac2d6e68
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 07:04:55 -0700

    Update and rename Brew.sh to Intstall.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Intstall.sh
+++ Intstall.sh
@@ -1,4 +1,3 @@
-
 bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 npm i --save genkit @genkit-ai/googleai
 pip install python-dotenv psycopg2
@@ -6,3 +5,4 @@ pip install vocos[train]
 pip install -r requirements.txt
 docker build -t kubu-hai .
 npm init builder.io@latest
+conda install -c conda-forge modal-client.
