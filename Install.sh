1187000c7b847e9613efc2a667eb0d6ec0841db4
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-25 05:49:59 -0700

    Update Install.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Install.sh
+++ Install.sh
@@ -1,3 +1,5 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 npm i --save genkit @genkit-ai/googleai
 pip install python-dotenv psycopg2
+pip install vocos[train]
