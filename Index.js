dda7689ada94418de50cd13577379df7ca749491
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-05 01:07:23 +0200

    Add files via upload
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- Index.js
+++ Index.js
@@ -1,35 +1,26 @@
-function createSimplePrompt() {
-  return { prompt: 'Tell me a joke about Cloudflare' };
-}
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
 
-function createChatMessages() {
-  return {
-    messages: [
-      { role: 'system', content: 'You are a helpful assistant.' },
-      { role: 'user', content: 'Who won the world series in 2020?' }
-    ]
-  };
-}
-
-async function processAIRequest(env, input) {
-  return await env.AI.run('@cf/meta/llama-3-8b-instruct', input);
-}
-
-export default {
-  async fetch(request, env) {
-    const tasks = [];
-    try {
-      let simple = createSimplePrompt();
-      let response = await processAIRequest(env, simple);
-      tasks.push({ inputs: simple, response });
-
-      let chat = createChatMessages();
-      response = await processAIRequest(env, chat);
-      tasks.push({ inputs: chat, response });
-
-      return new Response(JSON.stringify(tasks), { status: 200 });
-    } catch (error) {
-      return new Response(`Error: ${error.message}`, { status: 500 });
-    }
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <title>kubu-hai</title>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
