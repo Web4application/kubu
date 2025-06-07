db6a40cc3ee18f9b4ea65de17245dc7720ac18ba
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-07 06:31:16 +0200

    Update and rename bash to bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- backend/bash.sh
+++ backend/bash.sh
@@ -1,3 +1,6 @@
 python -m http.server 8000
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
 pip install -r requirements.txt
+cd kubu/backend
+pip install -r requirements.txt
+uvicorn main:app --reload
