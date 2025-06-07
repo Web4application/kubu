314418b8c04262102b06a288a0d1a3b838175738
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-07 07:33:26 +0200

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- backend/bash.sh
+++ backend/bash.sh
@@ -4,3 +4,4 @@ pip install -r requirements.txt
 cd kubu/backend
 pip install -r requirements.txt
 uvicorn main:app --reload
+docker pull ghcr.io/web4application/kubu:sha256-65a937d797bd4a24738624dc6d2e0f7a7e52ee300c0ed459c653674c212fe2d8.sig
