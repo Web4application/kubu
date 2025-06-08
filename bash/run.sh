25c71f8a7ed50fb03e4615a31cafa8c1f8628cac
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-08 21:47:07 +0200

    Update run.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash/run.sh
+++ bash/run.sh
@@ -1,23 +1,23 @@
-python main.py --repo ../kubu-hai
+python main.py --repo ../kubuverse
 pip install -r requirements.txt
 python -m http.server 8000
-cd kubu/backend
+
+cd kubuverse/backend
 pip install -r requirements.txt
 uvicorn main:app --reload
-cd kubu
-git add .
-git commit -m "Add audit script, CI/CD workflow, and FastAPI backend scaffold"
-git push origin main
-cd kubu/scripts
+
+cd kubuverse/scripts
 python3 audit_repo.p
+
 docker pull ghcr.io/web4application/kubu:main
 docker pull ghcr.io/web4application/kubu@sha256:<digest>
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
-docker build -t ghcr.io/web4application/kubu:main .
+docker build -t ghcr.io/web4application/kubuverse:main .
 # Generate key (only once)
 cosign generate-key-pair
 
 # Sign image
 cosign sign --key cosign.key ghcr.io/web4application/kubu:main
 cosign verify --key cosign.pub ghcr.io/web4application/kubu:main
+
 pip install fastapi uvicorn sqlalchemy psycopg2-binary
